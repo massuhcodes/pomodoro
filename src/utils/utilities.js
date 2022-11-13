c96cb9d7a182c3c0f51ed80cf447e4e24d6387e2
massuhcodes <massuhcodes@gmail.com>
2022-11-12 23:20:19 -0500

    Simplified code

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -35,12 +35,10 @@ export function openPomodoroSettings() {
 
 export function updateTypeDuration(type, action) {
     const typeDurationEl = document.getElementById(`${type}-duration`);
-    const amount = parseInt(typeDurationEl.textContent);
-    if (amount > 1) {
-        typeDurationEl.textContent =
-            parseInt(typeDurationEl.textContent) +
-            (action === "increment" ? 1 : -1);
-    }
+    const amount =
+        parseInt(typeDurationEl.textContent) +
+        (action === "increment" ? 1 : -1);
+    if (amount > 0) typeDurationEl.textContent = amount;
 }
 
 export function updateTypeChoice(type, choice) {
@@ -51,17 +49,11 @@ export function updateTypeChoice(type, choice) {
         if (type === "font") {
             typeChoice.style.backgroundColor = "#EFF1FA";
             typeChoice.style.color = "#1E213F";
-        } else {
-            typeChoice.innerHTML = "";
-        }
+        } else typeChoice.innerHTML = "";
     });
     // modify selected
     if (type === "font") {
         typeChoice.style.backgroundColor = "#161932";
         typeChoice.style.color = "#fff";
-    } else {
-        typeChoice.innerHTML = `
-            <img src=${checkmark} />
-        `;
-    }
+    } else typeChoice.innerHTML = `<img src=${checkmark} />`;
 }
